daa01ad8d8b97d33b69e940ef13042a9984b28a1
rafaw <rafa.willian91@gmail.com>
2023-04-13 05:45:29 -0300

    Copiando, Movendo e Renomeando Arquivos

---
+++ src/copiandoMovendoRenomeandoArquivos/MoveCopyRename.java
@@ -0,0 +1,30 @@
+package copiandoMovendoRenomeandoArquivos;
+
+import javax.imageio.IIOException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+public class MoveCopyRename {
+
+    public static void main(String[] args) throws IOException {
+
+        //Copiando arquivos
+        Path from = Path.of("src", "copiandoMovendoRenomeandoArquivos", "hello.txt");//Onde esta
+        Path p1 = from.getParent().resolve("ArqCopiado").resolve("Hello2.txt");//Para onde vai e o Nome
+       // Path p1 = from.getParent().resolve(Path.of("ArqCopiado").resolve("AqrCopiado.txt"));
+      //  Files.copy(from, p1);//Metodo para copia
+
+        //O java não deixa eu copiar o mesmo arquivo para um destino onde ele já existe, mas um enum pode ser
+        //passado para sobrescrever o arquivo existente se no ato da copia ele existir
+        Files.copy(from, p1, StandardCopyOption.REPLACE_EXISTING);
+
+        //Para mover é o mesmo, só que ele sobrescreve as informações contidas no arquivo, não o arquivo inteiro
+        Files.move(from, p1, StandardCopyOption.REPLACE_EXISTING);
+
+
+
+    }
+
+}
