732908e69d35e343bc2c7d63bb21ed1a10804ae7
rafaw <rafa.willian91@gmail.com>
2023-04-25 02:30:25 -0300

    Exercicio de copia de um txt para outra com um delimitador \\.

---
+++ src/Classes_Reader_Writter_Input_OutPut/exercise02/externalReader/ExternalReaderData.java
@@ -0,0 +1,30 @@
+package Classes_Reader_Writter_Input_OutPut.exercise02.externalReader;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class ExternalReaderData {
+
+    public static void main(String[] args) throws IOException {
+
+      try(
+              Scanner sc = new Scanner(Path.of("examples/FileWriter/dataIn.txt"));
+              PrintWriter pt = new PrintWriter("examples/FileWriter/dataOut.txt")) {
+
+          sc.useDelimiter("\\.");
+
+          while (sc.hasNext()) {
+              pt.println(sc.next().trim());
+          }
+      }
+
+
+    }
+
+}
