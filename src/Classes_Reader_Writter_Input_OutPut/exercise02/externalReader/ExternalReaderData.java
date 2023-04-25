4b6472a44894a591b622fcf43c446a0cb355484b
rafaw <rafa.willian91@gmail.com>
2023-04-25 03:25:10 -0300

    Exercicio de copia de um txt para outra com um delimitador \\.

--- src/Classes_Reader_Writter_Input_OutPut/exercise02/externalReader/ExternalReaderData.java
+++ src/Classes_Reader_Writter_Input_OutPut/exercise02/externalReader/ExternalReaderData.java
@@ -1,7 +1,6 @@
 package Classes_Reader_Writter_Input_OutPut.exercise02.externalReader;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.nio.file.Path;
 import java.util.Scanner;
 
@@ -9,15 +8,29 @@ public class ExternalReaderData {
 
     public static void main(String[] args) throws IOException {
 
+//      try(
+//              Scanner sc = new Scanner(Path.of("examples/FileWriter/dataIn.txt"));
+//              PrintWriter pt = new PrintWriter("examples/FileWriter/dataOut.txt")) {
+//
+//          sc.useDelimiter("\\.");
+//
+//          while (sc.hasNext()) {
+//              pt.println(sc.next().trim());
+//          }
+//      }
+
       try(
-              Scanner sc = new Scanner(Path.of("examples/FileWriter/dataIn.txt"));
-              PrintWriter pt = new PrintWriter("examples/FileWriter/dataOut.txt")) {
+              var in = new BufferedReader(new FileReader("examples/FileWriter/dataIn.txt"));
+              var out = new BufferedWriter(new FileWriter("examples/FileWriter/dataOut.txt"))
+              ){
 
-          sc.useDelimiter("\\.");
+          String line;
+          while ((line = in.readLine()) != null){
 
-          while (sc.hasNext()) {
-              pt.println(sc.next().trim());
+              line = line.replace(". ", "\n");
+              out.write(line);
           }
+
       }
 
 
